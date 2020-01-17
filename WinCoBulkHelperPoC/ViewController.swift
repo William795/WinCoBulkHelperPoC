20b1e9a3dd29147bc16564d17a09435dde100117
William Moody <oswin795@outlook.com>
2020-01-17 12:21:11 -0700

    First Commit

---
+++ WinCoBulkHelperPoC/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WinCoBulkHelperPoC
+//
+//  Created by William Moody on 1/17/20.
+//  Copyright © 2020 William Moody. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
